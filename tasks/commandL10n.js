c9db468b3e003db3e08d9f70886d1e0bcb6368e2
normanbarber <normanebarber@gmail.com>
2015-03-26 17:21:34 -0400

    using promises to handle large reads

--- tasks/commandL10n.js
+++ tasks/commandL10n.js
@@ -4,46 +4,21 @@ var commandL10n = require('../lib/commandL10n.js');
 var _ = require('lodash-node');
 var fs = require('fs');
 var walker = require('walkdir');
+var walkhelper = require('../lib/walker.js');
 
 module.exports = function (grunt) {
 	var helpers = {
 		pathCleanUp: function(filepath){
 			return filepath.replace(/\//g,"\\");
 		},
-		cleanUpViewLocales:function(matches){
-			return _.map(matches, function(match){
-				return match.replace(/\{__\(|\)\}|\\|'/g,"");
-			})
-		},
-		matchViewLocales:function(viewcode){
-			/*    method removes formatting from view code.
-			 only need the localization variables so it runs a match() for those and removes
-			 the underscores and curly braces and everything except for tha valued
-			 needed to compare with the en.json keys */
-			return _.map(viewcode, function(viewlocale) {
-				var code = viewlocale.replace(/"/g,"'");
-				var matches = code.match(/({__\(\'(.*)\')/g);
-				var viewlocalekeys = helpers.cleanUpViewLocales(matches);
-				return viewlocalekeys;
-			});
-
-		},
 		cleanEnJSONLocales:function(locales){
 			/*    method removes formatting from en.json code.
 			 only need the key to compare with the locale vars pulled from the view code */
-			var code = locales.replace(/\r|\n|\t|^\s+|\s+$|"|'|\{|\}/g,"");
-			var locales = code.split(',');
 			return _.map(locales, function(str) {
 				var locale = str.replace(/"/g,"'");
 				locale = locale.match(/(.*)[\:]/)[0].replace(':', '').replace(/^\s+|\s+$/g, "").replace(/\\r\\n|\\/g,"").replace(/^\s+|\s+$/g, "")
 				return locale;
 			});
-		},
-		findWidows:function(localekeys,viewkeyscombined){
-			/*    this method will find any key vars in the view that have not been added to the en.json
-			 option1 - automatically add them to the en.json using the var name for both the key/value
-			 option2 - log a warn message in the console */
-			return _.difference(viewkeyscombined,localekeys);
 		}
 	};
 
@@ -57,50 +32,16 @@ module.exports = function (grunt) {
 
 		var files = [];
 		var localecode = grunt.file.read(localefilepath).replace(/"/g,"'").replace(/\r|\n|\t|\{|\}/g,"");
-		var localearray = localecode.split(',');
-		var localedata =
-		{
-			viewcode:[],
-			viewkeys:[],
-			viewkeyscombined:[],
-			localekeys:[],
-			keysintersect:[],
-			widows:[]
-		};
+		var code = localecode.replace(/\r|\n|\t|^\s+|\s+$\{|\}/g,"");
+		var localearray = code.split(',');
+		var locales = helpers.cleanEnJSONLocales(localearray);
 
-		walker(this.data.view.path, function(filepath){
-			if(filepath){
-				fs.stat(filepath, function(error, data){
+		locales = _.map(locales, function(locale){
+			return locale.replace(/'|"/g,"")
+		});
 
-					if(!data.isDirectory()){
-						localedata.viewcode.push(grunt.file.read(filepath));
-						localedata.viewkeys = helpers.matchViewLocales(localedata.viewcode);
-						localedata.viewkeyscombined = _.flatten(localedata.viewkeys);
-
-						localedata.localekeys = helpers.cleanEnJSONLocales(JSON.stringify(localecode));
-
-						localedata.keysintersect = _.intersection(localedata.viewkeyscombined,localedata.localekeys);
-						localedata.widows = helpers.findWidows(localedata.localekeys,localedata.viewkeyscombined);
-						localecode = localecode.replace(/"/g,"'").replace(/\r|\n|\t|\{|\}/g,"");
-						localearray = localecode.split(',');
-						done();
-					}
-
-				})
-			}
-
-		})
-		.on('error',function(error){
-			grunt.log.error('The folder you are trying to read from is either misspelled or does not exist');
-			grunt.log.error('This is causing the error -> ' + error);
-		})
-		.on('end', function(){
-			if(!localedata.keysintersect || localedata.keysintersect.length <= 0){
-				grunt.log.error('Error');
-				grunt.log.error('no localization vars found in view files match the keys in your main localization file');
-				return false;
-			}
-			commandL10n(localedata.keysintersect, localearray, localefilepath, writefilepath)(grunt)
+		walkhelper(this.data.view.path,function(files){
+			commandL10n(files, locales, localearray, localefilepath, writefilepath)(grunt)
 				.fin(function(data){
 					console.log('\nresults returned and written successfully');
 					console.log('\nresults written to: ' + writefilepath);
@@ -109,10 +50,11 @@ module.exports = function (grunt) {
 
 				})
 				.fail(function(error){
-					console.log('errror');
+					console.log('error');
 				})
 
-		});
+		})
+
 
 	});
 
